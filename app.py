2f48197c5219c337977dd9e1da3d1202a0a00ba4
Nes890 <nesrinebenamor518@gmail.com>
2024-10-19 23:47:30 +0200

    Initialisation du projet avec Streamlit et OpenAI

--- app.py
+++ app.py
@@ -4,4 +4,17 @@ st.title("Plateforme de Recherche Médicale")
 question = st.text_input("Posez une question médicale")
 
 if question:
-    st.write(f"Vous avez posé la question : {question}")
+    answer = generate_answer(question)
+    st.write(f"Réponse : {answer}")
+
+import requests
+
+def search_pubmed(query):
+    url = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term={query}&retmode=json"
+    response = requests.get(url)
+    data = response.json()
+    return data["esearchresult"]["idlist"]
+
+if question:
+    pubmed_ids = search_pubmed(question)
+    st.write(f"Articles PubMed trouvés : {pubmed_ids}")
